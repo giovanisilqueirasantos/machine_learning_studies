87517bc539e47986b6e28412ad472dfee3514dbf
Giovani Silqueira Santos <giovani0skeey@gmail.com>
2019-09-08 12:34:59 -0300

    understanding svm

---
+++ SVM_sklearn_rbf_kernel_iris.py
@@ -0,0 +1,30 @@
+from sklearn import datasets
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from utils.graph import plot_decision_regions
+
+iris = datasets.load_iris()
+X = iris.data[:, [2, 3]]
+y = iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+svm = SVC(kernel='rbf', gamma=0.2, C=1.0, random_state=0)
+# svm = SVC(kernel='rbf', gamma=100.0, C=1.0, random_state=0)
+svm.fit(X_train_std, y_train)
+
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+plot_decision_regions(X=X_combined_std, y=y_combined, classifier=svm, test_idx=range(105, 150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.show()
