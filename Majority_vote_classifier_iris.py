9c09cb74222170f2f9579eb25012b4ea638905ac
Giovani Silqueira Santos <giovani0skeey@gmail.com>
2019-12-01 23:30:05 -0300

    Make a great model with simple models using majority vote idea

---
+++ Majority_vote_classifier_iris.py
@@ -0,0 +1,30 @@
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import Pipeline
+from utils.MajorityVoteClassifier import MajorityVoteClassifier
+import numpy as np
+
+iris = datasets.load_iris()
+X, y = iris.data[50:, [1, 2]], iris.target[50:]
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=1)
+
+clf1 = LogisticRegression(penalty='l2', C=0.001, random_state=0)
+clf2 = DecisionTreeClassifier(max_depth=1, criterion='entropy', random_state=0)
+clf3 = KNeighborsClassifier(n_neighbors=1, p=2, metric='minkowski')
+pipe1 = Pipeline([['sc', StandardScaler()], ['clf', clf1]])
+pipe3 = Pipeline([['sc', StandardScaler()], ['clf', clf3]])
+mv_clf = MajorityVoteClassifier(classifiers=[pipe1, clf2, pipe3])
+clf_labels = ['Logistic Regression', 'Decision Tree', 'KNN', 'Majority Votring']
+print('10-fold cross validation:\n')
+for clf, label in zip([pipe1, clf2, pipe3, mv_clf], clf_labels):
+    scores = cross_val_score(estimator=clf, X=X_train, y=y_train, cv=10, scoring='roc_auc')
+    print(f'ROC AUC: {scores.mean():.2f} (+/- {scores.std():.2f}) [{label}]')
