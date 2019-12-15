ccab3c22d7f6e0703ce1fb44b9e945b15d88c97f
Giovani Silqueira Santos <giovani0skeey@gmail.com>
2019-12-15 19:49:54 -0300

    visualize ROC_AUC plot and decision boundary from Majority vote classifier

--- Majority_vote_classifier_iris.py
+++ Majority_vote_classifier_iris.py
@@ -2,6 +2,8 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import roc_curve
+from sklearn.metrics import auc
 from sklearn.model_selection import cross_val_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
@@ -9,6 +11,8 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.pipeline import Pipeline
 from utils.MajorityVoteClassifier import MajorityVoteClassifier
 import numpy as np
+import matplotlib.pyplot as plt
+from itertools import product
 
 iris = datasets.load_iris()
 X, y = iris.data[50:, [1, 2]], iris.target[50:]
@@ -24,7 +28,46 @@ pipe1 = Pipeline([['sc', StandardScaler()], ['clf', clf1]])
 pipe3 = Pipeline([['sc', StandardScaler()], ['clf', clf3]])
 mv_clf = MajorityVoteClassifier(classifiers=[pipe1, clf2, pipe3])
 clf_labels = ['Logistic Regression', 'Decision Tree', 'KNN', 'Majority Votring']
+all_clf = [pipe1, clf2, pipe3, mv_clf]
 print('10-fold cross validation:\n')
-for clf, label in zip([pipe1, clf2, pipe3, mv_clf], clf_labels):
+for clf, label in zip(all_clf, clf_labels):
     scores = cross_val_score(estimator=clf, X=X_train, y=y_train, cv=10, scoring='roc_auc')
     print(f'ROC AUC: {scores.mean():.2f} (+/- {scores.std():.2f}) [{label}]')
+
+linestyles = [':', '--', '-.', '-']
+for clf, label, ls in zip(all_clf, clf_labels, linestyles):
+    y_pred = clf.fit(X_train, y_train).predict_proba(X_test)[:, 1]
+    fpr, tpr, thresholds = roc_curve(y_true=y_test, y_score=y_pred)
+    roc_auc = auc(x=fpr, y=tpr)
+    plt.plot(fpr, tpr, linestyle=ls, label=f'{label} (auc = {roc_auc:.2f})')
+
+plt.legend(loc='lower right')
+plt.plot([0, 1], [0, 1], linestyle='--', color='gray', linewidth=2)
+plt.xlim([-0.1, 1.1])
+plt.ylim([-0.1, 1.1])
+plt.grid()
+plt.xlabel('False positive rate')
+plt.ylabel('True positive rate')
+plt.show()
+
+sc = StandardScaler()
+X_train_std = sc.fit_transform(X_train)
+from itertools import product
+x_min = X_train_std[:, 0].min() - 1
+x_max = X_train_std[:, 0].max() + 1
+y_min = X_train_std[:, 1].min() - 1
+y_max = X_train_std[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1), np.arange(y_min, y_max, 0.1))
+f, axarr = plt.subplots(nrows=2, ncols=2, sharex='col', sharey='row', figsize=(7, 5))
+for idx, clf, tt in zip(product([0, 1], [1, 0]), all_clf, clf_labels):
+    clf.fit(X_train_std, y_train)
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    axarr[idx[0], idx[1]].contourf(xx, yy, Z, alpha=0.3)
+    axarr[idx[0], idx[1]].scatter(X_train_std[y_train==0, 0], X_train_std[y_train==0, 1], c='blue', marker='^', s=50)
+    axarr[idx[0], idx[1]].scatter(X_train_std[y_train==1, 0], X_train_std[y_train==1, 1], c='red', marker='o', s=50)
+    axarr[idx[0], idx[1]].set_title(tt)
+
+plt.text(-3.5, -4.5, s='Sepal width [standardized]', ha='center', va='center', fontsize=12)
+plt.text(-10.5, 4.5, s='Sepal length [standardized]', ha='center', va='center', fontsize=12, rotation=90)
+plt.show()
