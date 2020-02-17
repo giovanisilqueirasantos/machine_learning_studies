96a495dc4399b625af1e531de9e2abfe0ee922a7
Giovani Silqueira Santos <giovani0skeey@gmail.com>
2020-02-16 22:30:28 -0300

    learning about polynomial regression models for non-linear relationship

---
+++ polynomial_regression_housing.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.metrics import r2_score
+
+df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/housing/housing.data', header=None, sep='\s+')
+df.columns = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
+
+X = df[['LSTAT']].values
+y = df['MEDV'].values
+
+regr = LinearRegression()
+
+#create polynomial features
+quadratic = PolynomialFeatures(degree=2)
+cubic = PolynomialFeatures(degree=3)
+X_quad = quadratic.fit_transform(X)
+X_cubic = cubic.fit_transform(X)
+
+#linear fit
+X_fit = np.arange(X.min(), X.max(), 1)[:, np.newaxis]
+regr = regr.fit(X, y)
+y_lin_fit = regr.predict(X_fit)
+linear_r2 = r2_score(y, regr.predict(X))
+
+#quadratic fit
+regr = regr.fit(X_quad, y)
+y_quad_fit = regr.predict(quadratic.fit_transform(X_fit))
+quadratic_r2 = r2_score(y, regr.predict(X_quad))
+
+#cubic fit
+regr = regr.fit(X_cubic, y)
+y_cubic_fit = regr.predict(cubic.fit_transform(X_fit))
+cubic_r2 = r2_score(y, regr.predict(X_cubic))
+
+#plot results
+plt.scatter(X, y, label='training points', color='lightgray')
+plt.plot(X_fit, y_lin_fit, label=f'linear (d=1), $Rˆ2={linear_r2:.2f}', color='blue', lw=2, linestyle=':')
+plt.plot(X_fit, y_quad_fit, label=f'linear (d=2), $Rˆ2={quadratic_r2:.2f}', color='red', lw=2, linestyle='-')
+plt.plot(X_fit, y_cubic_fit, label=f'linear (d=3), $Rˆ2={cubic_r2:.2f}', color='green', lw=2, linestyle='--')
+plt.xlabel('% lower status of the population [LSTAT]')
+plt.ylabel('Price in $1000\'s [MEDV]')
+plt.legend(loc='upper right')
+plt.show()
